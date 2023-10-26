547e8b690f79912cac3e4183baef7ac37a5940c9
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-10-26 15:13:40 +0200

    BONUS request (file writer): DONE

--- src/org/lessons/java/list/Main.java
+++ src/org/lessons/java/list/Main.java
@@ -1,6 +1,11 @@
 package org.lessons.java.list;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,7 +15,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         // Creo ArrayList di regali
-        List<String> regali = new ArrayList<>();
+        List<Regalo> regali = new ArrayList<>();
 
         int Risposta = 0;
         boolean exit = false;
@@ -37,7 +42,7 @@ public class Main {
                         // Creo un nuovo oggetto Regalo passando come paramentri quelli dello scanner
                         Regalo regalo = new Regalo(descrizione, destinatario);
                         // Aggiungo il regalo all'ArrayList
-                        regali.add(String.valueOf(regalo));
+                        regali.add(regalo);
                         // Stampo lughezza lista e regalo appena creato
                         System.out.println("Lunghezza lista regali: " + regali.size());
                         System.out.println(regalo);
@@ -62,5 +67,51 @@ public class Main {
 
         // Chiudo lo scanner da tastiera
         scan.close();
+
+        // Inizializzo a null il FileWriter
+        FileWriter fileWriter = null;
+        try {
+            // Provo ad aprirlo (metto true per non sovrascrivere)
+            fileWriter = new FileWriter("./resources/data.txt", true);
+            // Provo a scriverci sopra
+            for (int i = 0; i < regali.size(); i++) {
+                fileWriter.write("Regalo");
+                fileWriter.write("\nDescrizione: " + regali.get(i).getDescrizione());
+                fileWriter.write("\nDestinatario: " + regali.get(i).getDestinatario() + "\n \n");
+            }
+        } // Se si solleva un'eccezione passo dal catch
+        catch (IOException e) {
+            System.out.println("Impossibile scrivere sul file.");
+        } // Sia che ho terminato il try, sia che sono entrato nel catch passo di qui
+        finally {
+            try {
+                // Se il fileWriter è diverso da null, lo chiudo
+                if (fileWriter != null) {
+                    System.out.println("Chiudo il file.");
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Provo a leggere il file
+        Scanner fileReader = null;
+        try {
+            // Setto file da leggere
+            fileReader = new Scanner(new File("./resources/data.txt"));
+            // Creo loop finchè viene trovata una nuova riga
+            while (fileReader.hasNextLine()) {
+                // Stampo riga
+                String line = fileReader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato.");
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
     }
 }
